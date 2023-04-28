5f8cb20acb8d8324aea4c5e7cf63bf391f74843b
shinya u <shinyahazama7@gmail.com>
2023-04-28 19:43:16 +0900

    change var name

--- main.js
+++ main.js
@@ -27,48 +27,47 @@ const ones = {
 	"9" : "sembilan "
 };
 
-function convertToIndonesian(number){
-	const integer = parseInt(number);
-	const digit = number.length;
-	const convert3Digits = (digit, word) => {
-		switch (digit){
+function convertToIndonesian(str_number){
+	const int_number = parseInt(str_number);
+	const number_of_digits = str_number.length;
+	const convert3Digits = (number_of_digits, word) => {
+		switch (number_of_digits){
 			case 1:
-				return (number[0] !== "1" ? ones[number[0]] + `${word} ` : `satu ${word} `) + convertToIndonesian(number.substring(1));
+				return (str_number[0] !== "1" ? ones[str_number[0]] + `${word} ` : `satu ${word} `) + convertToIndonesian(str_number.substring(1));
 			case 2:
-				return convertToIndonesian(number.substring(0, 2)) + `${word} `  + convertToIndonesian(number.substring(2));
+				return convertToIndonesian(str_number.substring(0, 2)) + `${word} `  + convertToIndonesian(str_number.substring(2));
 			case 3:
-				return convertToIndonesian(number.substring(0, 3)) + `${word} ` + convertToIndonesian("0" + number.substring(3));
+				return convertToIndonesian(str_number.substring(0, 3)) + `${word} ` + convertToIndonesian("0" + str_number.substring(3));
 		}
 		return "null";
 	}
 
-
-	if (number[0] === "0" && number.length > 1)
-		return convertToIndonesian(number.substring(1));
-	if (integer > 10 && integer < 20)
-		return ones[number[1]] + "belas ";
-	switch (digit){
+	if (str_number[0] === "0" && str_number.length > 1)
+		return convertToIndonesian(str_number.substring(1));
+	if (int_number > 10 && int_number < 20)
+		return ones[str_number[1]] + "belas ";
+	switch (number_of_digits){
 		case 1:
-			return integer !== 1 ? ones[integer] : "satu ";
+			return int_number !== 1 ? ones[int_number] : "satu ";
 		case 2:
 		case 3:
 		case 4:
-			return ones[number[0]] + ["puluh ", "ratus ", "ribu "][digit - 2] + convertToIndonesian(number.substring(1));
+			return ones[str_number[0]] + ["puluh ", "ratus ", "ribu "][number_of_digits - 2] + convertToIndonesian(str_number.substring(1));
 		case 5:
 		case 6:
-			return convert3Digits(digit - 3, "ribu");
+			return convert3Digits(number_of_digits - 3, "ribu");
 		case 7:
 		case 8:
 		case 9:
-			return convert3Digits(digit - 6, "juta");
+			return convert3Digits(number_of_digits - 6, "juta");
 		case 10:
 		case 11:
 		case 12:
-			return convert3Digits(digit - 9, "milyar");
+			return convert3Digits(number_of_digits - 9, "milyar");
 		case 13:
 		case 14:
 		case 15:
-			return convert3Digits(digit - 12, "trilyun");
+			return convert3Digits(number_of_digits - 12, "trilyun");
 	}
 	return "null";
 }
